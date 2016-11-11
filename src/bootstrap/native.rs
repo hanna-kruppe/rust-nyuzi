04b92c7b9fdd6018d759c34a3c79a6d5848560f5
Robin Kruppe <robin.kruppe@gmail.com>
2016-11-11 16:42:24 +0100

    Build Nyuzi backend, don't build JSBackend

--- src/bootstrap/native.rs
+++ src/bootstrap/native.rs
@@ -82,7 +82,7 @@ pub fn llvm(build: &Build, target: &str) {
     // NOTE: remember to also update `config.toml.example` when changing the defaults!
     let llvm_targets = match build.config.llvm_targets {
         Some(ref s) => s,
-        None => "X86;ARM;AArch64;Mips;PowerPC;SystemZ;JSBackend;MSP430;Sparc;NVPTX;Hexagon",
+        None => "X86;ARM;AArch64;Mips;PowerPC;SystemZ;MSP430;Sparc;NVPTX;Hexagon;Nyuzi",
     };
 
     let assertions = if build.config.llvm_assertions {"ON"} else {"OFF"};
--- src/librustc_llvm/build.rs
+++ src/librustc_llvm/build.rs
@@ -88,7 +88,7 @@ fn main() {
 
     let mut optional_components =
         vec!["x86", "arm", "aarch64", "mips", "powerpc", "pnacl",
-             "systemz", "jsbackend", "msp430", "sparc", "nvptx"];
+             "systemz", "msp430", "sparc", "nvptx", "nyuzi"];
 
     let mut version_cmd = Command::new(&llvm_config);
     version_cmd.arg("--version");
